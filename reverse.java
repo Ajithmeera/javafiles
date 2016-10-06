3cfcb4c2d3b50375b73568f3380d6baa49e2e255
Ajithmeera <ajithmeerayasiqmr@gmail.com>
2016-10-06 14:08:33 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,5 +1,10 @@
 #import java.util.*;
 class reverse
 {
-
+  public static void main(String arg[])
+  {
+    Scanner sc=new Scanner(System.in);
+    StringBuffer sb=new StringBuffer(sc.next());
+    System.out.println(sb.reverse());
+  }
 }
